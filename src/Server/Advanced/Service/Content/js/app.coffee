c8e9a479e406995240d78e55efaf9876fceff19f
chaliy <mike@chaliy.name>
2011-12-11 17:34:24 +0200

    Initial commit with knockjs version

--- src/Server/Advanced/Service/Content/js/app.coffee
+++ src/Server/Advanced/Service/Content/js/app.coffee
@@ -1,74 +1,16 @@
 $(document).ajaxError (ev, xhr, settings, errorThrown) ->
     alert(xhr.responseText)
 
-mainPanel = $("#main")
-body = $("body")
-currentPath = window.location.hash
+viewModel =    
+    folders: ['Inbox', 'Archive', 'Sent', 'Spam']
+    selectedFolder: ko.observable 'Inbox'
+    selectedMailId: ko.observable()
+    currentWorks: ko.observableArray([])
 
-# Utility
+    loadStats: (s) =>
+        viewModel.currentWorks(s.CurrentJobs)		
 
-showProgress = -> body.addClass("loading")
-hideProgress = -> body.removeClass("loading")
+window.worksViewModel = viewModel
+ko.applyBindings(viewModel)
 
-replaceMain = (tpl, data) ->
-    content = $(tpl).tmpl(data)
-    mainPanel.empty()
-    mainPanel.append(content)
-    
-refresh = ->
-    if (currentPath.indexOf("#work-details-") == 0)
-        key = currentPath.replace("#work-details-", "");
-        showWorkDetails key
-    else
-        showHome()
-
-route = (path) ->
-    currentPath = path
-    refresh()
-
-# Routes
-
-showHome = ->
-    showProgress()
-    $.ajax
-        url: "Stats?v=1"
-        context: document.body
-        dataType: "json"
-        cache: false
-        success: (data, status, xhr) ->            
-            replaceMain "#work-list-tmpl", data
-
-            $(".force-work-btn").click (btn) ->
-                workKey = $(btn.srcElement).data("work-key")
-                $.post("Work/#{workKey}/Force", () -> refresh())
-            
-            hideProgress()
-
-showWorkDetails = (key) ->
-    showProgress()
-    $.ajax
-        url: "Stats/Work/#{key}/?v=1"
-        context: document.body
-        dataType: "json"
-        cache: false
-        success: (data, status, xhr) ->            
-            replaceMain "#work-details-tmpl", data
-                        
-            hideProgress()    
-
-# Handlers
-
-$("#refresh-stats-btn").click -> refresh()
-
-$("#stop-btn").click ->
-    $.post "Stop", -> refresh()    
-
-$("#start-btn").click ->
-    $.post "Start", -> refresh()
-
-$(window).bind "hashchange", ->
-    currentPath = window.location.hash
-    refresh()
-
-
-refresh()
+ko.dependentObservable (-> $.get("Stats?v=1", null, viewModel.loadStats)), viewModel
--- src/Server/Advanced/Service/Content/js/app.js
+++ src/Server/Advanced/Service/Content/js/app.js
@@ -1,85 +1,21 @@
 (function() {
-  var body, currentPath, hideProgress, mainPanel, refresh, replaceMain, route, showHome, showProgress, showWorkDetails;
+  var viewModel;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   $(document).ajaxError(function(ev, xhr, settings, errorThrown) {
     return alert(xhr.responseText);
   });
-  mainPanel = $("#main");
-  body = $("body");
-  currentPath = window.location.hash;
-  showProgress = function() {
-    return body.addClass("loading");
+  viewModel = {
+    folders: ['Inbox', 'Archive', 'Sent', 'Spam'],
+    selectedFolder: ko.observable('Inbox'),
+    selectedMailId: ko.observable(),
+    currentWorks: ko.observableArray([]),
+    loadStats: __bind(function(s) {
+      return viewModel.currentWorks(s.CurrentJobs);
+    }, this)
   };
-  hideProgress = function() {
-    return body.removeClass("loading");
-  };
-  replaceMain = function(tpl, data) {
-    var content;
-    content = $(tpl).tmpl(data);
-    mainPanel.empty();
-    return mainPanel.append(content);
-  };
-  refresh = function() {
-    var key;
-    if (currentPath.indexOf("#work-details-") === 0) {
-      key = currentPath.replace("#work-details-", "");
-      return showWorkDetails(key);
-    } else {
-      return showHome();
-    }
-  };
-  route = function(path) {
-    currentPath = path;
-    return refresh();
-  };
-  showHome = function() {
-    showProgress();
-    return $.ajax({
-      url: "Stats?v=1",
-      context: document.body,
-      dataType: "json",
-      cache: false,
-      success: function(data, status, xhr) {
-        replaceMain("#work-list-tmpl", data);
-        $(".force-work-btn").click(function(btn) {
-          var workKey;
-          workKey = $(btn.srcElement).data("work-key");
-          return $.post("Work/" + workKey + "/Force", function() {
-            return refresh();
-          });
-        });
-        return hideProgress();
-      }
-    });
-  };
-  showWorkDetails = function(key) {
-    showProgress();
-    return $.ajax({
-      url: "Stats/Work/" + key + "/?v=1",
-      context: document.body,
-      dataType: "json",
-      cache: false,
-      success: function(data, status, xhr) {
-        replaceMain("#work-details-tmpl", data);
-        return hideProgress();
-      }
-    });
-  };
-  $("#refresh-stats-btn").click(function() {
-    return refresh();
-  });
-  $("#stop-btn").click(function() {
-    return $.post("Stop", function() {
-      return refresh();
-    });
-  });
-  $("#start-btn").click(function() {
-    return $.post("Start", function() {
-      return refresh();
-    });
-  });
-  $(window).bind("hashchange", function() {
-    currentPath = window.location.hash;
-    return refresh();
-  });
-  refresh();
+  window.worksViewModel = viewModel;
+  ko.applyBindings(viewModel);
+  ko.dependentObservable((function() {
+    return $.get("Stats?v=1", null, viewModel.loadStats);
+  }), viewModel);
 }).call(this);
