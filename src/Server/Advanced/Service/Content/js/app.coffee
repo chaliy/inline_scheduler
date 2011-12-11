a3b69d0dec425e645daa130ca43a9c1f55007f25
chaliy <mike@chaliy.name>
2011-12-11 23:48:02 +0200

    All stuff is now based on knowckoutjs

--- src/Server/Advanced/Service/Content/js/app.coffee
+++ src/Server/Advanced/Service/Content/js/app.coffee
@@ -5,24 +5,56 @@ viewModel =
     selectedWorkId: ko.observable()
     selectedWork: ko.observable()
     currentWorks: ko.observableArray([])
-    stats: ko.observable()	
+    
+    # Stats
     pendingJobsCount: ko.observable()
     scheduledJobsCount: ko.observable()
     runningJobsCount: ko.observable()
+    isStopped: ko.observable()
 
-    start: -> $.post "Start", -> viewModel.refresh()
-    stop: -> $.post "Stop", -> viewModel.refresh()
-    refresh: -> $.get("Stats?v=1", null, viewModel.loadStats)
+    # UI
+    inProgress: ko.observable()    
 
-    loadStats: (s) ->
-        viewModel.pendingJobsCount(s.PendingJobs)
-        viewModel.scheduledJobsCount(s.ScheduledJobs)
-        viewModel.runningJobsCount(s.RunningJobs)
-        viewModel.currentWorks(s.CurrentJobs)
+    # Actions
+    start: -> viewModel.post "Start"
+    stop: -> viewModel.post "Stop"
+    force: (workId) -> viewModel.post "Work/#{workId}/Force"
+
+    refresh: -> 
+        viewModel.inProgress(true)        
+        viewModel.get("Stats?v=1", (s) -> 
+            # Stats
+            viewModel.pendingJobsCount(s.PendingJobs)
+            viewModel.scheduledJobsCount(s.ScheduledJobs)
+            viewModel.runningJobsCount(s.RunningJobs)
+            viewModel.isStopped(s.IsStopped)
+            # Data
+            currentWorks = $.map(s.CurrentJobs, (j) ->
+                    j.force = -> viewModel.force(j.WorkKey)
+                    j )
+            viewModel.currentWorks(currentWorks)
+
+            viewModel.inProgress(false))
+
+    # Utils
+    post: (c) -> 
+        viewModel.inProgress(true)
+        $.post c, -> viewModel.refresh()
+
+    get: (u, c) ->
+        $.ajax
+            url: u
+            context: document.body
+            dataType: "json"
+            cache: false
+            success: (data, status, xhr) -> c(data)
 
 ko.dependentObservable (-> 
-	workIdFind = viewModel.selectedWorkId()
-	$.get("Stats/Work/#{workIdFind}/?v=1", null, viewModel.selectedWork)
+    workIdFind = viewModel.selectedWorkId()
+    if (workIdFind)
+	    viewModel.get("Stats/Work/#{workIdFind}/?v=1", viewModel.selectedWork)
+    else
+        viewModel.selectedWork null            
 	), viewModel
 
 window.worksViewModel = viewModel
--- src/Server/Advanced/Service/Content/js/app.js
+++ src/Server/Advanced/Service/Content/js/app.js
@@ -7,34 +7,64 @@
     selectedWorkId: ko.observable(),
     selectedWork: ko.observable(),
     currentWorks: ko.observableArray([]),
-    stats: ko.observable(),
     pendingJobsCount: ko.observable(),
     scheduledJobsCount: ko.observable(),
     runningJobsCount: ko.observable(),
+    isStopped: ko.observable(),
+    inProgress: ko.observable(),
     start: function() {
-      return $.post("Start", function() {
-        return viewModel.refresh();
-      });
+      return viewModel.post("Start");
     },
     stop: function() {
-      return $.post("Stop", function() {
+      return viewModel.post("Stop");
+    },
+    force: function(workId) {
+      return viewModel.post("Work/" + workId + "/Force");
+    },
+    refresh: function() {
+      viewModel.inProgress(true);
+      return viewModel.get("Stats?v=1", function(s) {
+        var currentWorks;
+        viewModel.pendingJobsCount(s.PendingJobs);
+        viewModel.scheduledJobsCount(s.ScheduledJobs);
+        viewModel.runningJobsCount(s.RunningJobs);
+        viewModel.isStopped(s.IsStopped);
+        currentWorks = $.map(s.CurrentJobs, function(j) {
+          j.force = function() {
+            return viewModel.force(j.WorkKey);
+          };
+          return j;
+        });
+        viewModel.currentWorks(currentWorks);
+        return viewModel.inProgress(false);
+      });
+    },
+    post: function(c) {
+      viewModel.inProgress(true);
+      return $.post(c, function() {
         return viewModel.refresh();
       });
     },
-    refresh: function() {
-      return $.get("Stats?v=1", null, viewModel.loadStats);
-    },
-    loadStats: function(s) {
-      viewModel.pendingJobsCount(s.PendingJobs);
-      viewModel.scheduledJobsCount(s.ScheduledJobs);
-      viewModel.runningJobsCount(s.RunningJobs);
-      return viewModel.currentWorks(s.CurrentJobs);
+    get: function(u, c) {
+      return $.ajax({
+        url: u,
+        context: document.body,
+        dataType: "json",
+        cache: false,
+        success: function(data, status, xhr) {
+          return c(data);
+        }
+      });
     }
   };
   ko.dependentObservable((function() {
     var workIdFind;
     workIdFind = viewModel.selectedWorkId();
-    return $.get("Stats/Work/" + workIdFind + "/?v=1", null, viewModel.selectedWork);
+    if (workIdFind) {
+      return viewModel.get("Stats/Work/" + workIdFind + "/?v=1", viewModel.selectedWork);
+    } else {
+      return viewModel.selectedWork(null);
+    }
   }), viewModel);
   window.worksViewModel = viewModel;
   ko.applyBindings(viewModel);
