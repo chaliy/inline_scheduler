0bfb858659aaacb536f0cd78d579f88b2953bedf
Josh Robb <josh_robb@fastmail.fm>
2012-06-13 14:49:03 +1200

    Add a flag to control if the server UI is running.

--- src/Server/InlineSchedulerServer.cs
+++ src/Server/InlineSchedulerServer.cs
@@ -8,29 +8,29 @@ namespace InlineScheduler.Server
 {
     public class InlineSchedulerServer
     {
-        public static void Init(Scheduler scheduler, string prefix = "Scheduler/")
+        public static void Init(Scheduler scheduler, string prefix = "Scheduler/", bool useServerUI = true)
         {
-            // Add / to the prefix, otherwise UI will not work            
-            prefix = prefix ?? "";
-            if (!prefix.EndsWith("/"))
-            {
-                prefix += "/";
+            if (useServerUI) {
+                // Add / to the prefix, otherwise UI will not work            
+                prefix = prefix ?? "";
+                if (!prefix.EndsWith("/")) {
+                    prefix += "/";
+                }
+
+                var config = new HttpConfiguration();
+                config.CreateInstance = (t, c, m) => new SchedulerService(scheduler);
+
+                config.Formatters.Remove(config.Formatters.JsonFormatter);
+                config.Formatters.Remove(config.Formatters.JsonValueFormatter);
+                config.Formatters.Insert(0, new JsonNetFormatter());
+
+                config.MaxReceivedMessageSize = 16777216; // 16M
+                config.MaxBufferSize = 16777216; // 16M
+
+                RouteTable.Routes.MapServiceRoute<SchedulerService>(prefix, config);
             }
-           
-        	var config = new HttpConfiguration();
-            config.CreateInstance = (t, c, m) => new SchedulerService(scheduler);
-
-            config.Formatters.Remove(config.Formatters.JsonFormatter);
-            config.Formatters.Remove(config.Formatters.JsonValueFormatter);
-            config.Formatters.Insert(0, new JsonNetFormatter());
-
-            config.MaxReceivedMessageSize = 16777216; // 16M
-            config.MaxBufferSize = 16777216; // 16M
-
-            RouteTable.Routes.MapServiceRoute<SchedulerService>(prefix, config);
 
             HostingRegistration.Register(scheduler);
-        }        
-
+        }
     }
 }
